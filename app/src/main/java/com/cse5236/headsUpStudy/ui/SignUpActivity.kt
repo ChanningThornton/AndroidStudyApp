01cf2ea79643216cb9c1cf49900674f8db20f7e1
ChanningThornton <channing@ironpulley.com>
2024-11-13 23:35:00 -0500

    Fixed bug with signup

--- app/src/main/java/com/cse5236/headsUpStudy/ui/SignUpActivity.kt
+++ app/src/main/java/com/cse5236/headsUpStudy/ui/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.cse5236.headsUpStudy.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Debug
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -32,7 +33,7 @@ class SignUpActivity : AppCompatActivity(), View.OnClickListener {
             }
             R.id.signup_button -> {
                 val email = findViewById<EditText>(R.id.editTextUsername)
-                val password = findViewById<EditText>(R.id.editTextTextPassword)
+                val password = findViewById<EditText>(R.id.editTextPassword)
                 signUpViewModel.signUp(email.text.toString(), password.text.toString())
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
